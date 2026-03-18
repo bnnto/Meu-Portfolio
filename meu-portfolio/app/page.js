584cf0b94474bf73e56cf58a5916e63f073b7a3a
Bento Guilherme <gbento555@gmail.com>
2026-03-18 16:25:51 -0300

    carrosel de skills joia

--- meu-portfolio/app/page.js
+++ meu-portfolio/app/page.js
@@ -6,31 +6,67 @@ export default function Home() {
       <div className={styles.barra}>
         <a href="#sobre">Sobre mim</a>
         <a href="#projetos">Projetos</a>
-        <a href="#experiencias">Experiências</a>
+        <a href="#contato">Contato</a>
       </div>
 
       <section id="sobre" className={styles.sobre}>
-        <img src="/foto.jpeg" alt="Minha Foto" className={styles.imgFluid}></img>
+        <div className={styles.fotoSociais}>
+          <img src="/foto.jpeg" alt="Minha Foto" className={styles.imgFluid}></img>
+
+          <div className={styles.divSociais}>
+              <a href="https://github.com/bnnto" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
+                <img src="/github.svg"/>
+              </a>
+              <a href="https://www.linkedin.com/in/bento-guilherme/" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
+                <img src="/linkedin.svg"/>
+              </a>
+              <a href="mailto:gbento555@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
+                <img src="/email.svg"/>
+              </a>
+              <a href="/Currículo-BentoGuilherme.pdf" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
+                <img src="/curriculo.svg"/>
+              </a>
+            </div>
+          </div>
+          
         <div className={styles.sobreInfo}>
           <h2>Bento Guilherme Gomes Oliveira</h2>
           <p>Estudante de Ciência da Computação - <a href="https://portal.unicap.br/" target="_blank" rel="noopener noreferrer">Universidade Católica de Pernambuco</a></p>
           <p>Atualmente estudando e aprimorando minhas habilidades nas áreas que gosto (Back e Front End).</p>
-          <p>Python, C, C++, Java, MySQL, HTML, CSS, JavaScript, Django, Android Studio e mais.</p>
 
-          <div className={styles.divSociais}>
-            <a href="https://github.com/bnnto" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
-              <img src="/github.svg"/>
-            </a>
-            <a href="https://www.linkedin.com/in/bento-guilherme/" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
-              <img src="/linkedin.svg"/>
-            </a>
-            <a href="mailto:gbento555@gmail.com" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
-              <img src="/email.svg"/>
-            </a>
-            <a href="/Currículo-BentoGuilherme.pdf" target="_blank" rel="noopener noreferrer" className={styles.minhasSociais}>
-              <img src="/curriculo.svg"/>
-            </a>
-          </div>
+          <section id="skills" className={styles.skills}>
+            <div className={styles.carrossel}>
+              <div className={styles.tecnol}>
+                <img src="/python.svg"/>
+                <img src="/kotlin.png"/>
+                <img src="/java.svg"/>
+                <img src="/c++.svg"/>
+                <img src="/django.png"/>
+                <img src="/react.svg"/>
+                <img src="/linux.svg"/>
+                <img src="/git.svg"/>
+                <img src="/figma.svg"/>
+                <img src="/js.svg"/>
+                <img src="/raspb.svg"/>
+                <img src="/cnormal.svg"/>
+                <img src="/css.svg"/>
+
+                <img src="/python.svg"/>
+                <img src="/kotlin.png"/>
+                <img src="/java.svg"/>
+                <img src="/c++.svg"/>
+                <img src="/django.png"/>
+                <img src="/react.svg"/>
+                <img src="/linux.svg"/>
+                <img src="/git.svg"/>
+                <img src="/figma.svg"/>
+                <img src="/js.svg"/>
+                <img src="/raspb.svg"/>
+                <img src="/cnormal.svg"/>
+                <img src="/css.svg"/>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
 
